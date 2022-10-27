46afdeb65c12698eb92fa91b6ce7d4b68a34dbed
ljlinux <47410354+ljlinux@users.noreply.github.com>
2022-10-27 14:06:21 +0530

    Update startup_script.cmd

--- scripts/startup_script.cmd
+++ scripts/startup_script.cmd
@@ -1,1 +1,3 @@
-powerShell -windowstyle hidden C:\temp\windows_precheck.ps1 >> “C:\temp\startup_log_file.log”
+@echo off
+ 
+powershell.exe -ExecutionPolicy Unrestricted -Command ". 'C:\temp\windows_precheck.ps1'" >> "C:\temp\startup_log_file.log" 2>&1
